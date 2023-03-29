19eb8d03e9a77c6f7d12669740d2b6ff373baa7b
VitaliyVT <ToropchinVV@yandex.ru>
2023-03-29 20:09:04 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    }
-  }
-})
+    },
+  },
+});
